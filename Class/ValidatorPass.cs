c71d24488d215540260b49bc502d5e1692a478df
augustodasneves <augustodasneves@hotmail.com>
2015-03-31 17:08:11 -0300

    Update ValidatorPass.cs
    alterada sequencia

--- Class/ValidatorPass.cs
+++ Class/ValidatorPass.cs
@@ -20,14 +20,50 @@ namespace AppGerenciaSenhas.Class
     {
         protected ForcaQuesito[] Forca = new ForcaQuesito[5];
 
-        public void ValidaSenha(string pass){
+        public string ValidaSenha(string pass)
+        {
             VerificaTamanho(pass);
             VerificaTipoCaractere(pass);
             VerificaDicionario(pass);
             VerificaSequencia(pass);
             VerificaDatas(pass);
 
+            int cFraco = 0;
+            int cMedia = 0;
+            int cForte = 0;
+
+            for (int i = 0; i < Forca.Length; i++)
+            {
+                if (Forca[i] == ForcaQuesito.Fraca)
+                {
+                    cFraco++;
+                }
+                else if (Forca[i] == ForcaQuesito.Média)
+                {
+                    cMedia++;
+                }
+                else if (Forca[i] == ForcaQuesito.Forte)
+                {
+                    cForte++;
+                }
+
+            }
+
+            if (cFraco >= cMedia && cFraco >= cForte)
+            {
+                return "Fraco";
+            }
+            else if (cMedia >= cFraco && cMedia >= cForte)
+            {
+                return "Média";
+            }
+            else if (cForte >= cFraco && cForte >= cMedia)
+            {
+                return "Forte";
+            }
+            return "";
         }
+
         private void VerificaTamanho(string password)
         {
             if (password.Length <= 3)
@@ -47,7 +83,7 @@ namespace AppGerenciaSenhas.Class
         private void VerificaTipoCaractere(string password)
         {
             int val = 0;
-             //MINUSCULA
+            //MINUSCULA
             if (Regex.Match(password, @"[a-z]").Success)
             {
                 val += 1;
@@ -81,45 +117,104 @@ namespace AppGerenciaSenhas.Class
             }
         }
 
-        private async void VerificaDicionario(string password)
+        private /*async*/ void VerificaDicionario(string password)
         {
+
+            StreamReader sr = new StreamReader(@"~\..\..\..\Assets\dict_passes.csv");
+            int cP = 0;
+            foreach (var fv in sr.ReadLine())
+            {
+                if (password.Equals(fv))
+                {
+                    cP++;
+                }
+            }
+            if (cP == 0)
+            {
+                Forca[2] = ForcaQuesito.Forte;
+            }
+
             // settings
             // same as (ms-appx:///MyFolder/MyFile.txt)
-            var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            /*var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
             _Folder = await _Folder.GetFolderAsync("Assets");
 
             // acquire file
             var _File = await _Folder.GetFileAsync("dict_passes.csv");
             
             // read content
-            var _ReadThis = await Windows.Storage.FileIO.ReadTextAsync(_File);
+            var _ReadThis = await Windows.Storage.FileIO.ReadTextAsync(_File);*/
 
         }
 
         private void VerificaSequencia(string password)
         {
-            
+            int i = 0;
+            int j = 0;
+            foreach (var letra in password)
+            {
+                if (i > 0)
+                {
+                    if(password[i-1].Equals(password[i])){
+                        j++;
+                    }
+                }
+                i++;
+            }
+            if (j > 0)
+            {
+                Forca[3] = ForcaQuesito.Fraca;
+            }
+            else
+            {
+                i = 0;
+                j = 0;
+                foreach (var letra in password)
+                {
+                    if (i > 0)
+                    {
+                        if ((ASCIIEncoding.ASCII.GetBytes(password[i - 1].ToString())[0]+1).Equals(ASCIIEncoding.ASCII.GetBytes(password[i].ToString())[0]))
+                        {
+                            j++;
+                        }
+                    }
+                    i++;
+                }
+                if (j > 0)
+                {
+                    Forca[3] = ForcaQuesito.Fraca;
+                }
+            }
         }
 
         private void VerificaDatas(string password)
         {
-            CultureInfo provider=CultureInfo.InvariantCulture;
-            try{
+            CultureInfo provider = new CultureInfo("pt-BR");
+            try
+            {
                 DateTime t;
-                DateTime.ParseExact(password, "ddmmaaaa", provider);
-                Forca[4]=ForcaQuesito.Fraca;
-                
+                DateTime.ParseExact(password, "ddMMyy", provider);
+                Forca[4] = ForcaQuesito.Fraca;
+
                 return;
             }
-            finally { }
-            try{
+            catch
+            {
+                System.Diagnostics.EventLog.WriteEntry("teste", "erro processa ddmmyy");
+            }
+
+            try
+            {
                 DateTime t;
-                DateTime.ParseExact(password,"ddmmaa",provider);
-                Forca[4]=ForcaQuesito.Média;
+                DateTime.ParseExact(password, "ddMMyyyy", provider);
+                Forca[4] = ForcaQuesito.Média;
                 return;
             }
-            finally {  }
-            Forca[4] = ForcaQuesito.Forte;               
+            catch
+            {
+                System.Diagnostics.EventLog.WriteEntry("teste", "erro processa ddmmyyyy");
+            }
+            Forca[4] = ForcaQuesito.Forte;
         }
     }
 }
